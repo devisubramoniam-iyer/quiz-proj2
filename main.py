f1c8888fd77fd67c34391b92b5c29bce8a9aaa3c
devisubramoniam-iyer <devisubramoniam@gmail.com>
2025-11-14 13:39:17 +0530

    Add files via upload
    main.py added

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Define the structure of the request body
+class Item(BaseModel):
+    message: str
+
+@app.post("/echo")
+def echo_data(item: Item):
+    return {"you_sent": item}
