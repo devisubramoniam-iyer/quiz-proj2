43871433ac9f628e9dc8dd9f6ece7fe874ac5aff
devisubramoniam-iyer <devisubramoniam@gmail.com>
2025-11-16 15:27:49 +0530

    new commit

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
 
 app = FastAPI()
 
-# Define the structure of the request body
-class Item(BaseModel):
-    message: str
+# Read secret from environment variable
+SECRET = os.getenv("SECRET_KEY")
 
-@app.post("/echo")
-def echo_data(item: Item):
-    return {"you_sent": item}
+class QuizRequest(BaseModel):
+    email: str
+    secret: str
+    url: str
+
+
+@app.post("/")
+def handle_quiz(data: QuizRequest):
+
+    # If environment var missing → throw error
+    if SECRET is None:
+        raise HTTPException(
+            status_code=500,
+            detail="Server misconfiguration: SECRET_KEY not set."
+        )
+
+    # Check secret
+    if data.secret != SECRET:
+        raise HTTPException(status_code=403, detail="Forbidden: Invalid secret")
+
+    return {
+        "message": "Request received successfully",
+        "email": data.email,
+        "url": data.url
+    }
